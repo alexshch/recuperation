60fed1165c8d89d257b0e8602348cdb6d715764f
Alex Scherbakov <alexander.v.shcherbakov@gmail.com>
2017-11-28 17:27:25 +0300

    реализован выбор спискак компонентов газа

--- RecuperationGui/ViewModel/MainWindowViewModel.cs
+++ RecuperationGui/ViewModel/MainWindowViewModel.cs
@@ -1,44 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using RecuperationGui.Model;
 
 namespace RecuperationGui.ViewModel
 {
     internal class MainWindowViewModel : INotifyPropertyChanged
     {
-
+        public string ModelState { get; set; }
+        public bool ModelIsValid { get; set; }
         private ApplicationModel _applicationModel;
+        public ICommand UpdateSelectedGas1ScriptCommand { get; set; }
+        public ICommand UpdateSelectedGas2ScriptCommand { get; set; }
 
         public MainWindowViewModel()
         {
             _applicationModel = ApplicationModel.GetAplicationModel();
-            _compositionItems = new List<GasCompositionItem>
+            UpdateSelectedGas1ScriptCommand = new RelayCommand(UpdateSelectedGas1);
+            UpdateSelectedGas2ScriptCommand = new RelayCommand(UpdateSelectedGas2);
+
+            _gas1CompositionItems = new List<GasCompositionItem>
             {
                 new GasCompositionItem("вода", 25),
                 new GasCompositionItem("огонь", 25),
                 new GasCompositionItem("воздух", 25),
-                new GasCompositionItem("земля", 25)
+                new GasCompositionItem("земля", 25),
+                new GasCompositionItem("перец", 46)
+            };
+            _gas2CompositionItems = new List<GasCompositionItem>
+            {
+                new GasCompositionItem("бутан",23),
+                new GasCompositionItem("пропан",23),
+                new GasCompositionItem("ботан",23),
             };
         }
-        public List<GasCompositionItem> GasCompositionItems
+
+        private void UpdateSelectedGas1(object selectedProps)
         {
-            get { return _compositionItems; }
+            var selectedGasList = ((IList)selectedProps).Cast<GasCompositionItem>().ToList();
+            var gasMix = string.Join("\n", selectedGasList.Select(s => $"{s.Name,-8}: {s.Value}%"));
+            ChosenGas1Components = gasMix;
+        }
+
+        private void UpdateSelectedGas2(object selectedProps)
+        {
+            var selectedGasList = ((IList)selectedProps).Cast<GasCompositionItem>().ToList();
+            var gasMix = string.Join("\n", selectedGasList.Select(s => $"{s.Name,-8}: {s.Value}%"));
+            ChosenGas2Components = gasMix;
+
+        }
+
+        private List<GasCompositionItem> _gas1CompositionItems;
+        public List<GasCompositionItem> Gas1CompositionItems
+        {
+            get { return _gas1CompositionItems; }
             set
             {
-                if (value == _compositionItems) return;
-                _compositionItems = value;
+                if (value == _gas1CompositionItems) return;
+                _gas1CompositionItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _chosenGas1Components;
+        public string ChosenGas1Components
+        {
+            get { return _chosenGas1Components; }
+            set
+            {
+                if (value == _chosenGas1Components) return;
+                _chosenGas1Components = value;
+                Task.Run(() => SetScripts());
                 OnPropertyChanged();
             }
         }
 
 
+        private List<GasCompositionItem> _gas2CompositionItems;
+        public List<GasCompositionItem> Gas2CompositionItems
+        {
+            get { return _gas2CompositionItems; }
+            set
+            {
+                if (value == _gas2CompositionItems) return;
+                _gas2CompositionItems = value;
+                OnPropertyChanged();
+            }
+        }
 
-        private List<GasCompositionItem> _compositionItems;
+        private string _chosenGas2Components;
+        public string ChosenGas2Components
+        {
+            get { return _chosenGas2Components; }
+            set
+            {
+                if (value == _chosenGas2Components) return;
+                _chosenGas2Components = value;
+                Task.Run(() => SetScripts());
+                OnPropertyChanged();
+            }
+        }
+
+        private void SetScripts()
+        {
+            ModelState = "Проверка пользовательских скриптов...";
+
+            ModelIsValid = true;
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
